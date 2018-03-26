3a407e246a44d5c08f8c00620bc94b714a84a997
jiangzhenjing <1475149929@qq.com>
2018-03-26 19:51:39 +0800

    。。

--- ecloud-admin/sys-admin-interface/src/main/java/com/sunesoft/ecloud/adminclient/dtos/MenuDto.java
+++ ecloud-admin/sys-admin-interface/src/main/java/com/sunesoft/ecloud/adminclient/dtos/MenuDto.java
@@ -73,10 +73,108 @@ public class MenuDto {
     private MenuDto parentMenu;
 
 
-    private List<MenuFunctionDto> menuFunctions;
+    public UUID getUuid() {
+        return uuid;
+    }
 
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
 
+    public ModuleDto getModule() {
+        return module;
+    }
 
+    public void setModule(ModuleDto module) {
+        this.module = module;
+    }
 
+    public String getMenuIndex() {
+        return menuIndex;
+    }
+
+    public void setMenuIndex(String menuIndex) {
+        this.menuIndex = menuIndex;
+    }
+
+    public String getRouteCode() {
+        return routeCode;
+    }
+
+    public void setRouteCode(String routeCode) {
+        this.routeCode = routeCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public MenuType getType() {
+        return type;
+    }
+
+    public void setType(MenuType type) {
+        this.type = type;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFrontDisc() {
+        return frontDisc;
+    }
+
+    public void setFrontDisc(String frontDisc) {
+        this.frontDisc = frontDisc;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public Integer getChildCount() {
+        return childCount;
+    }
+
+    public void setChildCount(Integer childCount) {
+        this.childCount = childCount;
+    }
+
+    public MenuDto getParentMenu() {
+        return parentMenu;
+    }
+
+    public void setParentMenu(MenuDto parentMenu) {
+        this.parentMenu = parentMenu;
+    }
 
 }
