6fbac30e3f37f94d4895335afba62cf0fe084b8f
jiangzj <jiangzj@sunesoft.com>
2018-03-26 19:54:52 +0800

    包引入

--- ecloud-admin/sys-admin/src/main/java/com/sunesoft/ecloud/admin/service/AgencyService.java
+++ ecloud-admin/sys-admin/src/main/java/com/sunesoft/ecloud/admin/service/AgencyService.java
@@ -1,7 +1,9 @@
 package com.sunesoft.ecloud.admin.service;
 
 import com.sunesoft.ecloud.adminclient.dtos.AgencyDto;
-import com.sunesoft.ecloud.common.result.TResult
+import com.sunesoft.ecloud.common.result.TResult;
+
+import java.util.UUID;
 
 /**
  * @author: Zhouzh
@@ -11,9 +13,8 @@ public interface AgencyService {
 
     TResult addOrUpdateAgency(AgencyDto agencyDto);
 
-
-
     TResult delete(UUID id);
+
 
 
 
