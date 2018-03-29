7ac91a16fa92c0a245462e0ae6ebda1974390931
zhouzh <zhouzh@sunesoft.com>
2018-03-29 15:14:47 +0800

    修改结构

--- auth-server/auth-client/src/main/java/com/sunesoft/ecloud/auth/configs/CorsConfig.java
+++ auth-server/auth-client/src/main/java/com/sunesoft/ecloud/auth/configs/CorsConfig.java
@@ -1,10 +1,12 @@
 package com.sunesoft.ecloud.auth.configs;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@ComponentScan("com.sunesoft.ecloud")
 public class CorsConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
