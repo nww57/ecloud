babb9611548ad4c8b2e032da6ea8276dd3655fd7
niww <674022412@qq.com>
2018-04-04 14:40:30 +0800

    新增组织架构参数验证

--- ecloud-admin/sys-admin/src/main/java/com/sunesoft/ecloud/admin/repository/AgencyOrganizationRepository.java
+++ ecloud-admin/sys-admin/src/main/java/com/sunesoft/ecloud/admin/repository/AgencyOrganizationRepository.java
@@ -2,6 +2,7 @@ package com.sunesoft.ecloud.admin.repository;
 
 import com.sunesoft.ecloud.admin.domain.agency.AgencyOrganization;
 import com.sunesoft.ecloud.hibernate.repository.BaseRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +14,7 @@ import java.util.UUID;
  * @Auther: niww
  * @Date: 2018/3/27/027
  */
-public interface AgencyOrganizationRepository extends BaseRepository<AgencyOrganization,UUID> {
+public interface AgencyOrganizationRepository extends BaseRepository<AgencyOrganization,UUID>,JpaSpecificationExecutor {
 
     @Modifying
     @Query(value = "delete from AgencyOrganization where id in :ids")
@@ -24,4 +25,13 @@ public interface AgencyOrganizationRepository extends BaseRepository<AgencyOrgan
     @Modifying
     @Query(value = "update AgencyOrganization a set a.parentOrg = null where a.parentOrg.id = :pid")
     void updateParentAgencyNull(@Param("pid") UUID pid);
+
+
+    AgencyOrganization findByAgency_IdAndNameEquals(UUID agId,String name);
+
+    AgencyOrganization findByAgency_IdAndIdNotAndNameEquals(UUID agId,UUID id,String name);
+
+    AgencyOrganization findByAgency_IdAndCodeEquals(UUID agId,String code);
+
+    AgencyOrganization findByAgency_IdAndIdNotAndCodeEquals(UUID agId,UUID id,String code);
 }
