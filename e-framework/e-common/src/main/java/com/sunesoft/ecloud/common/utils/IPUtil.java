5f8e74fe91031bd171afb7f953be7f1404425efc
zhouzh <zhouzh@sunesoft.com>
2018-04-12 13:49:27 +0800

    新增权限验证

--- e-framework/e-common/src/main/java/com/sunesoft/ecloud/common/utils/IPUtil.java
+++ e-framework/e-common/src/main/java/com/sunesoft/ecloud/common/utils/IPUtil.java
@@ -1,38 +1,38 @@
-//package com.sunesoft.ecloud.common.utils;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-///**
-// * @Auther: niww
-// * @Date: 2018/4/10/010
-// */
-//public class IPUtil {
-//    /**
-//     * 获取客户机的ip地址
-//     * @param request
-//     * @return
-//     */
-//    public static String getClientIp(HttpServletRequest request) {
-//        String ip = request.getHeader("x-forwarded-for");
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-//            ip = request.getHeader("Proxy-Client-IP");
-//        }
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-//            ip = request.getHeader("WL-Proxy-Client-IP");
-//        }
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-//            ip = request.getRemoteAddr();
-//        }
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-//            ip = request.getHeader("http_client_ip");
-//        }
-//        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-//            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-//        }
-//        // 如果是多级代理，那么取第一个ip为客户ip
-//        if (ip != null && ip.indexOf(",") != -1) {
-//            ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
-//        }
-//        return ip;
-//    }
-//}
+package com.sunesoft.ecloud.common.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Auther: niww
+ * @Date: 2018/4/10/010
+ */
+public class IPUtil {
+    /**
+     * 获取客户机的ip地址
+     * @param request
+     * @return
+     */
+    public static String getClientIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("http_client_ip");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        // 如果是多级代理，那么取第一个ip为客户ip
+        if (ip != null && ip.indexOf(",") != -1) {
+            ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
+        }
+        return ip;
+    }
+}
