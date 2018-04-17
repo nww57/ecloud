ffcb65a0074963abcb4b68e17a6ad96af04289af
niww <674022412@qq.com>
2018-04-17 18:39:34 +0800

    去掉无用的agId

--- ecloud-admin/sys-admin-interface/src/main/java/com/sunesoft/ecloud/adminclient/clientService/UserServiceClient.java
+++ ecloud-admin/sys-admin-interface/src/main/java/com/sunesoft/ecloud/adminclient/clientService/UserServiceClient.java
@@ -1,20 +1,17 @@
 package com.sunesoft.ecloud.adminclient.clientService;
 
-import com.sunesoft.ecloud.adminclient.dtos.MenuFunctionDto;
 import com.sunesoft.ecloud.adminclient.dtos.UserDto;
-import com.sunesoft.ecloud.common.result.ListResult;
+import com.sunesoft.ecloud.adminclient.interceptor.FeignConfigInterceptor;
 import com.sunesoft.ecloud.common.result.TResult;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "sys-admin",url = "localhost:5010")
-public interface AuthServiceClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/admin/auth/authfunc/{userId}")
-    ListResult<MenuFunctionDto> getFunctions(@PathVariable("userId")String userId);
+import java.util.UUID;
 
+@FeignClient(name = "sys-admin",url = "localhost:5010",configuration = FeignConfigInterceptor.class)
+public interface UserServiceClient {
     @RequestMapping(method = RequestMethod.GET, value = "/admin/agency/user/realandrolename")
-    TResult<UserDto> getUserRealNameAndRoleName();
-
+    TResult<UserDto> getUserRealNameAndRoleName(@PathVariable("userId")UUID userId);
 }
