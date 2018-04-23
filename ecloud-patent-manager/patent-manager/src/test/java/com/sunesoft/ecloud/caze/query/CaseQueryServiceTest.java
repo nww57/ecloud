28115ca21df2d58ab298c36f1365a1e152752c1f
niww <674022412@qq.com>
2018-04-23 18:29:28 +0800

    测试类提交

--- ecloud-patent-manager/patent-manager/src/test/java/com/sunesoft/ecloud/caze/query/CaseQueryServiceTest.java
+++ ecloud-patent-manager/patent-manager/src/test/java/com/sunesoft/ecloud/caze/query/CaseQueryServiceTest.java
@@ -1,11 +1,13 @@
 package com.sunesoft.ecloud.caze.query;
 
+import com.sunesoft.ecloud.caseclient.PatentType;
 import com.sunesoft.ecloud.caseclient.criterias.CaseInfoQueryCriteria;
 import com.sunesoft.ecloud.caseclient.dto.CaseCustomerRequestDto;
 import com.sunesoft.ecloud.caseclient.dto.CaseInfoListDto;
 import com.sunesoft.ecloud.caseclient.dto.CaseMessageListDto;
 import com.sunesoft.ecloud.common.cretiria.TCretiria;
 import com.sunesoft.ecloud.common.result.PagedResult;
+import com.sunesoft.ecloud.common.result.TResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,12 +33,15 @@ public class CaseQueryServiceTest {
     public void queryCaseInfoByPaged() throws Exception {
         CaseInfoQueryCriteria criteria  = new CaseInfoQueryCriteria();
         criteria.setAgId(UUID.fromString("01d134d6-3eef-4b60-9979-c6392554da25"));
+        criteria.setPatentType(PatentType.INVENTION_PATENT);
         PagedResult<CaseInfoListDto> result = caseQueryService.queryCaseInfoByPaged(criteria);
         System.out.println(23);
     }
 
     @Test
     public void queryCaseBasicInfoById() throws Exception {
+        TResult<CaseInfoListDto> result = caseQueryService.queryCaseBasicInfoById(UUID.fromString("a4c8362b-9e8e-4a08-a1eb-71ebd87e86da"));
+        System.out.println(23);
     }
 
     @Test
@@ -47,7 +52,7 @@ public class CaseQueryServiceTest {
 
     @Test
     public void queryCaseMessageByPaged() throws Exception {
-        PagedResult<CaseMessageListDto> result = caseQueryService.queryCaseMessageByPaged(UUID.fromString("9cac8eaf-cd2d-4a9d-81ef-2526a71bd5fe"),new TCretiria());
+        PagedResult<CaseMessageListDto> result = caseQueryService.queryCaseMessageByPaged(UUID.fromString("9805ae93-3b2e-4cc8-b575-69fba4e87616"),new TCretiria());
         System.out.println(23);
     }
 
