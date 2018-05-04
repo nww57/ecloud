90ee04aec132959020bb8d9ae7fcbb28e97be9a2
niww <674022412@qq.com>
2018-05-04 18:18:03 +0800

    测试类提交

--- ecloud-patent-manager/patent-manager/src/test/java/com/sunesoft/ecloud/caze/query/impl/ContractQueryServiceImplTest.java
+++ ecloud-patent-manager/patent-manager/src/test/java/com/sunesoft/ecloud/caze/query/impl/ContractQueryServiceImplTest.java
@@ -1,9 +1,46 @@
+package com.sunesoft.ecloud.caze.query.impl;
+
+import com.sunesoft.ecloud.caseclient.criterias.ContractQueryCriteria;
+import com.sunesoft.ecloud.caseclient.dto.ContractDetailDto;
+import com.sunesoft.ecloud.caseclient.dto.ContractDto;
+import com.sunesoft.ecloud.caze.query.ContractQueryService;
+import com.sunesoft.ecloud.common.result.PagedResult;
+import com.sunesoft.ecloud.common.result.TResult;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.UUID;
+
 import static org.junit.Assert.*;
 
 /**
  * @Auther: niww
  * @Date: 2018/5/4/004
  */
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 public class ContractQueryServiceImplTest {
+
+    @Autowired
+    ContractQueryService contractQueryService;
+
+    private static final UUID AGID = UUID.fromString("01d134d6-3eef-4b60-9979-c6392554da25");
+    @Test
+    public void queryContractPaged() throws Exception {
+        ContractQueryCriteria criteria  =new ContractQueryCriteria();
+        criteria.setPageIndex(0);
+        criteria.setAgId(AGID);
+        PagedResult<ContractDto> list = contractQueryService.queryContractPaged(criteria);
+        System.out.println(23);
+    }
+
+    @Test
+    public void getContractDetailById() throws Exception {
+        TResult<ContractDetailDto> result = contractQueryService.getContractDetailById(UUID.fromString("d58827df-6f5a-421a-a935-3c2102a48e87"));
+        System.out.println(23);
+    }
 
 }
--- ecloud-patent-manager/patent-manager/src/test/java/com/sunesoft/ecloud/caze/service/impl/PatentServiceTest.java
+++ ecloud-patent-manager/patent-manager/src/test/java/com/sunesoft/ecloud/caze/service/impl/PatentServiceTest.java
@@ -1,9 +1,51 @@
+package com.sunesoft.ecloud.caze.service.impl;
+
+import com.sunesoft.ecloud.adminclient.AgencyType;
+import com.sunesoft.ecloud.caseclient.dto.CreateContractPatentDto;
+import com.sunesoft.ecloud.caseclient.enums.PatentType;
+import com.sunesoft.ecloud.caze.service.PatentService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.UUID;
+
 import static org.junit.Assert.*;
 
 /**
  * @Auther: niww
  * @Date: 2018/5/4/004
  */
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 public class PatentServiceTest {
+
+    @Autowired
+    PatentService patentService;
+
+    private static final UUID AGID = UUID.fromString("01d134d6-3eef-4b60-9979-c6392554da25");
+
+    @Test
+    public void addPatent() throws Exception {
+        CreateContractPatentDto dto = new CreateContractPatentDto();
+        dto.setAgId(AGID);
+        dto.setCaseNo("AJ201805040003");
+        dto.setContractId(UUID.fromString("d58827df-6f5a-421a-a935-3c2102a48e87"));
+        dto.setPatentType(PatentType.UTILITYMODEL_PATENT);
+        dto.setPatentName("测试专利");
+        dto.setCreatorId(UUID.fromString("fc98e1ca-92f0-4f2e-b68c-f0129978c1bf"));
+        patentService.addPatent(dto);
+        System.out.println(23);
+    }
+
+    @Test
+    public void deletePatent() throws Exception {
+    }
+
+    @Test
+    public void deletePatent1() throws Exception {
+    }
 
 }
