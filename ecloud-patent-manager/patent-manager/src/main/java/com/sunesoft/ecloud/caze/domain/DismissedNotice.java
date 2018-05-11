f9cbde9b998b5f662eb44f938c6ebb725cc74473
niww <674022412@qq.com>
2018-05-11 13:15:25 +0800

    字段添加

--- ecloud-patent-manager/patent-manager/src/main/java/com/sunesoft/ecloud/caze/domain/DismissedNotice.java
+++ ecloud-patent-manager/patent-manager/src/main/java/com/sunesoft/ecloud/caze/domain/DismissedNotice.java
@@ -121,4 +121,20 @@ public class DismissedNotice extends BizEntity {
     public void setSendFileDate(LocalDate sendFileDate) {
         this.sendFileDate = sendFileDate;
     }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 }
